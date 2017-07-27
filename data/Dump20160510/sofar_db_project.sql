e8aad022ebaa05044f5a3ead6041f6c087390170
KoDeokyoon <dykoh@finotek.co.kr>
2017-07-27 11:42:57 +0900

    V2.0
    add readme.md

--- data/Dump20160510/sofar_db_project.sql
+++ data/Dump20160510/sofar_db_project.sql
@@ -40,11 +40,6 @@ CREATE TABLE `project` (
 -- Dumping data for table `project`
 --
 
-LOCK TABLES `project` WRITE;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
-INSERT INTO `project` VALUES (29,'ATM','ATM','','2015-03-10 14:46:42','',1,NULL),(30,'OSS','Online Shopping System','','2015-03-18 15:10:49','H.Gomaa',1,NULL),(31,'PRS','Payroll system','','2015-03-23 11:09:02','anonymous',1,NULL),(32,'cos','Cafeteria ordering system','','2015-03-24 13:01:40','HMS',1,NULL),(33,'POS','POS System','from Applying uml and patterns','2015-04-09 12:44:29','',1,NULL),(34,'UIS','대학정보시스템','','2015-04-15 15:10:22','채흥석',1,NULL),(35,'TMS','Traffic Management','OOAD with App thrid edition','2015-04-15 17:51:25','',1,NULL),(36,'STS','Stock Trading System','','2015-05-19 11:08:33','A.Cockburn',1,NULL),(37,'OPS','Opder processing system','','2015-05-20 12:11:23','IBM',1,NULL),(38,'BABY','아기용 쇼핑몰','아기용 쇼핑몰','2015-11-03 18:22:00','A class',0,NULL),(39,'WEBS','웹쇼핑몰','','2015-11-04 20:37:02','쿠크다스',0,NULL),(40,'SKP','상품관리','','2015-11-10 11:50:49','SKT',1,NULL),(41,'META','모바일 컨텐츠 관리 시스템','','2015-11-11 15:40:52','SKT',1,NULL),(42,'AMT_T','test atm','','2015-12-21 21:39:47','',0,NULL),(43,'PAY','Online Payment System','','2016-02-11 15:54:46','KDY',1,NULL);
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
---
+++ readme.md
@@ -0,0 +1,14 @@
+This is software specification writing tool
+with automatic engine for recommendation of omitted steps of scenario
+via machine learning with training scenario set.
+
+for your installation
+1. install MySQL and restore data(/data) and view
+2. import code
+3. set root password and database name in the code
+ - src/MySQLDataAccessor/DataAccessString.java
+ - src/MySQLDataAccessor/MySqlConnectionCreator.java
+4. download reference libraries from
+ https://drive.google.com/drive/folders/0B6iblRUtrgVnanBRZzRNR0RpaDA
+5. register reference library in the IDE
+6. run it.
